fc0ef1126fd0843d11190fd62b3327752d5dffce
Paul Monnery <paul.monnery@epitech.eu>
2020-10-03 19:12:30 +0200

    feat: get current route in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Header = () : JSX.Element => (
-  <div className="header">
-    <ul className="navbar">
-      <Link to="/" style={{ textDecoration: 'none' }}>
-        <li className="nav-element">
-          <a>idea</a>
-        </li>
-      </Link>
-      <Link to="/product" style={{ textDecoration: 'none' }}>
-        <li className="nav-element">
-          <a>product</a>
-        </li>
-      </Link>
-      <Link to="/contact" style={{ textDecoration: 'none' }}>
-        <li className="nav-element">
-          <a>contact</a>
-        </li>
-      </Link>
-    </ul>
-  </div>
-);
+const Header = () : JSX.Element => {
+  const location = useLocation();
+
+  return (
+    <div className="header">
+      <ul className="navbar">
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <li className={location.pathname === '/' ? 'nav-element-current' : 'nav-element'}>
+            <span>idea</span>
+          </li>
+        </Link>
+        <Link to="/product" style={{ textDecoration: 'none' }}>
+          <li className={location.pathname === '/product' ? 'nav-element-current' : 'nav-element'}>
+            <span>product</span>
+          </li>
+        </Link>
+        <Link to="/contact" style={{ textDecoration: 'none' }}>
+          <li className={location.pathname === '/contact' ? 'nav-element-current' : 'nav-element'}>
+            <span>contact</span>
+          </li>
+        </Link>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;

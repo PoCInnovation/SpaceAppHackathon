1ae951a1a96cb3900a6b2271f7bca44773ad738a
Paul Monnery <paul.monnery@epitech.eu>
2020-10-04 01:07:34 +0200

    merged scroll arrow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { Header } from '../components/Header';
-import Arrow from '../../public/down-arrow.svg';
 
 const Container = styled.div`
   background: black;
@@ -30,7 +29,7 @@ const LogoView = styled.a`
   z-index: 3;
 `;
 
-const ArrowView = styled.div`
+const ArrowView = styled.a`
   position: absolute;
   bottom: 40px;
   left: 50%;
@@ -56,13 +55,19 @@ const SubTitle = styled.h2`
 `;
 
 function Home(): JSX.Element {
+  const endRef = useRef<HTMLDivElement>(document.createElement('div'));
+
   return (
     <Container style={{ flexDirection: 'column', height: window.innerHeight * 1.9 }}>
       <Header />
-      <LogoView href="https://github.com/PoCInnovation" rel="noreferrer" target="_blank">
+      <LogoView
+        href="https://github.com/PoCInnovation"
+        rel="noreferrer"
+        target="_blank"
+      >
         <img alt="poc" src="/logo.png" style={{ width: '20%', height: '20%' }} />
       </LogoView>
-      <Container style={{ flexDirection: 'row', width: '100%', height: '50%', marginTop: '5%', backgroundImage: 'url(/star.png)', position: 'relative' }}>
+      <Container style={{ flexDirection: 'row', width: '100%', height: '50%', marginTop: '5%', backgroundImage: 'url(/star.png)', backgroundAttachment: 'fixed', position: 'relative' }}>
         <LeftView>
           <Title style={{ marginLeft: '10%', marginTop: '10%' }}>Test</Title>
           <SubTitle style={{ marginLeft: '10%' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt fringilla augue, at pulvinar tellus egestas ac. Cras suscipit viverra tortor non interdum. Donec efficitur volutpat lacinia. </SubTitle>
@@ -72,16 +77,16 @@ function Home(): JSX.Element {
             <img alt="astronaute" src="/astro_1.png" style={{ width: '90%', height: '90%' }} />
           </Astro>
         </RightView>
-        <ArrowView>
+        <ArrowView onClick={() => endRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })}>
           <img alt="arrow" src="/down-arrow.svg" width="50" height="50" />
         </ArrowView>
       </Container>
       <Container style={{ flexDirection: 'row', height: '50%', color: 'white', backgroundImage: 'url(/moon.png)', backgroundSize: 'cover', backgroundPosition: 'bottom', backgroundRepeat: 'no-repeat  ' }}>
         <LeftView style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <iframe title="video" width="700" height="405" src="https://www.youtube.com/embed/pVddPNSzjZI"  allowFullScreen />
+          <iframe title="video" width="700" height="405" src="https://www.youtube.com/embed/pVddPNSzjZI" allowFullScreen />
         </LeftView>
         <RightView>
-          <Astro style={{ right: '10%', top: '5%' }}>
+          <Astro style={{ right: '10%', top: '5%' }} ref={endRef}>
             <img alt="astronaute" src="/astro_2.png" style={{ width: '90%', height: '90%' }} />
           </Astro>
         </RightView>

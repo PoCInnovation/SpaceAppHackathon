ec526a1577a90b0c1b224887b563f1642217fc4a
Fringhian <quentin.blackfoot.io>
2020-10-03 20:59:45 +0200

    feat: update files

--- src/Components/ModelRender/index.tsx
+++ src/Components/ModelRender/index.tsx
@@ -4,17 +4,23 @@ import { Canvas, useLoader } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 function Duck() {
-  const gltf = useLoader(GLTFLoader, 'Duck.glb');
+  const gltf = useLoader(GLTFLoader, 'Duck.gltf');
   return <primitive object={gltf.scene} position={[0, 0, 0]} />;
 }
 
 export default function ModelRender(): JSX.Element {
   return (
     <div className="Duck">
-      <Canvas camera={{ position: [0, 0, 10] }}>
+      <Canvas camera={{ position: [10, 10, 10] }}>
         <ambientLight intensity={0.5} />
         <spotLight intensity={0.8} position={[300, 300, 400]} />
-        <Suspense fallback={null}><Duck /></Suspense>
+        <Suspense fallback={null}>
+          <mesh
+            scale={[10, 10, 10]}
+          >
+            <Duck />
+          </mesh>
+        </Suspense>
       </Canvas>
     </div>
   );

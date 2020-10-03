22a391df1ca79424d3b9ec099fb57cea426f4b92
Paul Monnery <paul.monnery@epitech.eu>
2020-10-03 20:01:36 +0200

    add: black background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import { Header } from '../components/Header';
+
+const Container = styled.div`
+  background: black;
+  display: flex;
+  border: none;
+  flex-direction: column;
+`;
 
 function Home(): JSX.Element {
   return (
-    <div>
+    <Container style={{ height: window.innerHeight }}>
       <Header />
-      <p>Home</p>
-    </div>
+      <p style={{ color: 'white' }}>Home</p>
+    </Container>
   );
 }
 

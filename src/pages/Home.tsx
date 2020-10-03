d991081c02c87b71930080873b4ff507e095c75f
Pr0m3th3usEx <thomas1.michel@epitech.eu>
2020-10-03 19:32:26 +0200

    Feat: Add display astronaute and Titles home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,26 +6,50 @@ const Container = styled.div`
   background: black;
   display: flex;
   border: none;
+  overflow-x: hidden;
 `;
 
 const LeftView = styled.div`
-    width: 60%;
+    width: 50%;
     height: 100%;
 `;
 
 const RightView = styled.div`
-    width: 40%;
+    width: 50%;
     height: 100%;
+    position: relative;
+`;
+
+const Astro = styled.div`
+  position: absolute;
+`;
+
+const Title = styled.h1`
+  font-size: 5em;
+  z-index: 2; 
+  color: white;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 2em;
+  z-index: 2;
+  color: white;
+  text-align: justify;
 `;
 
 function Home(): JSX.Element {
   return (
     <Container style={{ flexDirection: 'column', height: window.innerHeight }}>
       <Header />
-      <Container style={{ flexDirection: 'row', width: '100%', height: '100%' }}>
-        <LeftView />
+      <Container className="first-astro" style={{ flexDirection: 'row', width: '100%', height: '100%' }}>
+        <LeftView>
+            <Title style={{ marginLeft: '10%', marginTop: '10%' }}>Test</Title>
+            <SubTitle style={{ marginLeft: '10%' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt fringilla augue, at pulvinar tellus egestas ac. Cras suscipit viverra tortor non interdum. Donec efficitur volutpat lacinia. </SubTitle>
+        </LeftView>
         <RightView>
-          <img alt="astronaute" src="/astro_1.png" style={{ width: '100%', height: '100%' }} />
+          <Astro style={{ right: '10%' }}>
+            <img alt="astronaute" src="/astro_1.png" />
+          </Astro>
         </RightView>
       </Container>
     </Container>

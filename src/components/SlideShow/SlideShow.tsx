1ae6c2840087310bad381b6ec99bf73ae28db13f
Pr0m3th3usEx <thomas1.michel@epitech.eu>
2020-10-04 11:35:35 +0200

    Feat: add slideshow

--- src/components/SlideShow/SlideShow.tsx
+++ src/components/SlideShow/SlideShow.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+import './SlideShow.css';
 
-const SlideShow = (props) : JSX.Element => {
+const SlideShow = () : JSX.Element => {
+  const height = window.innerHeight * 1.9 / 2;
 
+  return (
+    <div>
+      <Slide easing="ease">
+        <div className="each-slide" style={{ height }}>
+          <div style={{ backgroundColor: 'red', height: '100%' }}>
+            <span>Some text</span>
+          </div>
+        </div>
+        <div className="each-slide" style={{ height }}>
+          <div style={{ backgroundColor: 'yellow', height: '100%' }}>
+            <span>Some text 2</span>
+          </div>
+        </div>
+      </Slide>
+    </div>
+  );
 };
 
 export default SlideShow;
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Header } from '../components/Header';
-
+import { SlideShow } from '../components/SlideShow';
 
 const Container = styled.div`
   background: #2f3437;
@@ -16,8 +16,11 @@ function Product(): JSX.Element {
   const height = window.innerHeight;
 
   return (
-    <Container style={{ flexDirection: 'column', height: window.innerHeight }}>
+    <Container style={{ flexDirection: 'column', height: window.innerHeight * 2 - 111 }}>
       <Header />
+      <Container style={{ flexDirection: 'column', width: '100%', height: '100%', marginTop: '111px' }}>
+        <SlideShow />
+      </Container>
     </Container>
   );
 }

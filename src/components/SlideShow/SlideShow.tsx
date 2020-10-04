9b020db7092db2285453dad80804e223c2568be8
Pr0m3th3usEx <thomas1.michel@epitech.eu>
2020-10-04 12:08:03 +0200

    Fix Debug slideshow

--- src/components/SlideShow/SlideShow.tsx
+++ src/components/SlideShow/SlideShow.tsx
@@ -10,12 +10,12 @@ const SlideShow = () : JSX.Element => {
   return (
     <div>
       <Slide easing="ease">
-        <div className="each-slide" style={{ height }}>
+        <div className="each-slide" style={{ height, backgroundColor: 'yellow' }}>
           <div style={{ height: '100%', color: 'white'}}>
             <p>TEs 1</p>
           </div>
         </div>
-        <div className="each-slide" style={{ height }}>
+        <div className="each-slide" style={{ height, backgroundColor: 'red' }}>
           <div style={{ height: '100%' }}>
             <p>Test 2</p>
           </div>

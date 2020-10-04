c7f3137b3ad59b57cc6a37a02099f06dad93f01d
Paul Monnery <paul.monnery@epitech.eu>
2020-10-05 00:19:29 +0200

    add: title to slide

--- src/components/SlideShow/SlideShow.tsx
+++ src/components/SlideShow/SlideShow.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import './SlideShow.css';
-
+import styled from 'styled-components';
 import MySlide from '../Slide';
 import slidesJson from './slides.json';
 
+const Title = styled.h3`
+  margin-top: 4em;
+  margin-bottom: -2.5em;
+  font-size: 2em;
+  color: white;
+  width: 70%;
+`;
+
 const SlideShow = (): JSX.Element => (
   <div style={{ marginLeft: '10%', marginRight: '10%' }}>
+    <Title>
+      What types of modules can Caelus propose?
+    </Title>
     <Slide easing="ease" autoplay={false} duration={0}>
       {
           slidesJson.map((item) => (
